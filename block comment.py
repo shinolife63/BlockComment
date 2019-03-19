cbf1cbec0e2e271fe5fd29b226af7a2b7d2f00b6
Douglas Reilly <shinolife63@gmail.com>
2019-03-19 16:58:18 -0400

    Add files via upload

--- block comment.py
+++ block comment.py
@@ -6,46 +6,58 @@ def chunkSplit(l, n):
             yield l[i:i + n]
     return [' '.join(chunk) for chunk in chunker()]
 
-
-def blockComment(code,comment,justification = None, clinesize = 5):
-    justificationtoggle = 1
-    if justification == None:
-        commentdividend = 1
-        justificationtoggle = 0
-    elif justification == 'center':
-        commentdividend = 2
-    elif justification == 'right':
-        commentdividend = 1
-    
+def deblockComment(code):
+    lines = code.split('\n')
     output = []
+    for line in lines:
+        output.append('#'.join(line.split('#')[:-1]).rstrip())
+    return '\n'.join(output)
+
+def blockComment(code,comment,justification = None, clinesize = 7):
+    
+    justificationtoggle = 1                  # <---+    
+    if justification == None:                #     |    justificationtoggle being zero will cause no extra
+        commentdividend = 1                  #     |    spaces to be appended to comment_append, commentdividend
+        justificationtoggle = 0              #     |    determines the kind applied if justification is
+    elif justification == 'center':          #     +--> infact desired, will default to left. 
+        commentdividend = 2                  #     |    All this is doing is setting up
+    elif justification == 'right':           #     |    for the comment appending stage.
+        commentdividend = 1                  # <---+    
+    
+    
     lines = code.split('\n')
     longestlen = max([len(line) if '\t' not in line else len(line)+4*line.count('\t') for line in lines])+20
-    
     codecenter = int(len(lines)/2)
-    words = comment.split(' ')
-    
-    commentlines = chunkSplit(words, clinesize)
+
+    if '\n' not in comment:                                  # <---+    Checks if the comment already has linebreaks,
+        words = comment.split(' ')                           #     |    if not it breaks the comment into
+        commentlines = chunkSplit(words, clinesize)          #     +--> lines composed of clinesize'd words, and if
+    else:                                                    #     |    it does, simply uses the existing line breaks.
+        commentlines = comment.split('\n')                   # <---+    
+
+        
     longestcommentline = max([len(line) for line in commentlines])
-    #commentlines = [line.ljust(longestcommentline-len(line),' ') for line in commentlines]
-    commentcenter = int(len(chunkSplit(words, 5))/2)
+    commentcenter = int(len(commentlines)/2)
 
     queue = deque(commentlines)
+    output = []
     
-    for index, line in enumerate(lines):
-        if index >= codecenter-commentcenter:
-            try:
-                comment_append = queue.popleft()
-            except:
-                comment_append = ''
-        else:
-            comment_append = ''
-        #print(longestcommentline-len(comment_append))
-        if index == 0 or index == len(lines)-1:
-            output.append(line+' # <---+    '.rjust(longestlen-len(line)+1,' ')+' '*(int((longestcommentline-len(comment_append))/commentdividend)*justificationtoggle)+comment_append)
-        elif index == codecenter:
-            output.append(line+' #     +--> '.rjust(longestlen-len(line)+1,' ')+' '*(int((longestcommentline-len(comment_append))/commentdividend)*justificationtoggle)+comment_append)
-        else:
-            output.append(line+' #     |    '.rjust(longestlen-len(line)+1,' ')+' '*(int((longestcommentline-len(comment_append))/commentdividend)*justificationtoggle)+comment_append)
+    for index, line in enumerate(lines):                                                                                                 # <---+    
+        if (index >= codecenter-commentcenter) and queue:                                                                                #     |    
+            comment_append = queue.popleft()                                                                                             #     |    codecenter-commentcenter is the point where the comment should begin in order to align with the codeblock's center
+        else:                                                                                                                            #     +--> if index(the line currently being worked on) had reached that point, and the queue is not empty, comment_append is set to the next value from the queue
+            comment_append = ''                                                                                                          #     |    justificationtoggle being zero will cause no extra spaces to be appended to comment_append, commentdividend determines the kind applied if justification is infact desired, will default to left
+        comment_append = ' '*(int((longestcommentline-len(comment_append))/commentdividend)*justificationtoggle)+comment_append          # <---+    
+
+
+        if index == 0 or index == len(lines)-1:                                                          # <---+    
+            output.append(line+' # <---+    '.rjust(longestlen-len(line)+1,' ')+comment_append)          #     |    Because this operates on the codeblock line
+        elif index == codecenter:                                                                        #     |    by line, the output is an array
+            output.append(line+' #     +--> '.rjust(longestlen-len(line)+1,' ')+comment_append)          #     +--> we append the modified lines to, and
+        else:                                                                                            #     |    then convert back to a multiline string
+            output.append(line+' #     |    '.rjust(longestlen-len(line)+1,' ')+comment_append)          # <---+    right before returning.
+
+    
     return '\n'.join(output)
 
 def main():
